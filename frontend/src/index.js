3989b9de2d9c4672c4691a0fa917d8bd2f344c87
Ryokuman <ryokuman@naver.com>
2022-11-08 00:39:06 +0900

    feature (style) : reset.css , default.css
    add reset.css to reset styles
    add default.css to add background

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import { store } from "./stores";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "@assets/css/reset.css";
+import "@assets/css/default.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
