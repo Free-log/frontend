8989c4126093d80f03acab8f3e96050ae29f63dd
Ryokuman <ryokuman21@gmail.com>
2022-11-11 19:13:02 +0900

    chore ( pages ) : Login.js
    add codes for test

--- frontend/src/pages/Account/Login.js
+++ frontend/src/pages/Account/Login.js
@@ -1,12 +1,20 @@
 import File from "../../components/File";
+import styled from "styled-components";
+
+const TestDIV = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  overflow: hidden;
+`;
 
 function Login() {
   return (
-    <div>
+    <TestDIV>
       <File type="1" />
       <File type="2" />
       <File />
-    </div>
+    </TestDIV>
   );
 }
 
