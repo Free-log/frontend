962d1ed45d32fc14b771388a839c46703a3f1e81
Ryokuman <ryokuman21@gmail.com>
2022-11-12 23:22:09 +0900

    feature ( components ) : Box
    add fadeOut animation

--- frontend/src/components/Box.js
+++ frontend/src/components/Box.js
@@ -12,25 +12,42 @@ const Box = styled.div`
   opacity: ${(props) => (props.fadeIn ? 0 : 1)};
 
   ${(props) => {
-    if (props.fadeIn) {
+    if (props.fadeOut) {
+      return css`
+        animation-name: fadeout;
+        animation-duration: 1s;
+        animation-fill-mode: forwards;
+      `;
+    } else if (props.fadeIn) {
       return css`
         animation-name: fadein;
         animation-duration: ${(props) => props.fadeIn};
         animation-delay: ${(props) => props.delay};
         animation-fill-mode: forwards;
-
-        @keyframes fadein {
-          from {
-            margin-top: 100px;
-          }
-          to {
-            opacity: 1;
-            margin-top: 0;
-          }
-        }
       `;
     }
   }}
+
+  @keyframes fadein {
+    from {
+      margin-top: 100px;
+    }
+    to {
+      opacity: 1;
+      margin-top: 0;
+    }
+  }
+
+  @keyframes fadeout {
+    from {
+      opacity: 1;
+      margin-top: 0;
+    }
+    to {
+      opacity: 0;
+      margin-top: 100px;
+    }
+  }
 `;
 
 export default Box;
