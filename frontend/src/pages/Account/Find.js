3a3605b474551922d58c6a4e9a89fcbda73cd1b4
Ryokuman <ryokuman21@gmail.com>
2022-11-12 18:26:28 +0900

    chore ( page ) : finish find

--- frontend/src/pages/Account/Find.js
+++ frontend/src/pages/Account/Find.js
@@ -1,34 +1,95 @@
 import { TextField, Typography, Box, CustomForm, Button, StyledLink } from "@components";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { findIdValidator, findPasswordValidator, spaceRemover } from "@utils";
+
+function FindIdBox() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ mode: "onBlur", resolver: yupResolver(findIdValidator) });
+
+  const watcher = watch(["email"]);
+  const inputChecker = watcher.includes("") || watcher.includes(undefined) || Object.values(errors)[0];
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    // 여기다가 이제 백에 데이터 보내는거
+  };
+
+  return (
+    <Box position>
+      <CustomForm onSubmit={handleSubmit(onSubmit)}>
+        <Typography marginBottom="4px">EMAIL</Typography>
+        <TextField
+          marginBottom={errors.email?.message ? "6px" : "20px"}
+          onChange={spaceRemover}
+          {...register("email")}
+        />
+        <Typography margin="0px" color="#DF5659" size="14px">
+          {errors.email?.message}
+        </Typography>
+        <Button marginTop="20px" marginBottom="40px" disabled={inputChecker}>
+          FIND ID
+        </Button>
+      </CustomForm>
+    </Box>
+  );
+}
+
+function FindPasswordBox() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ mode: "onBlur", resolver: yupResolver(findPasswordValidator) });
+
+  const watcher = watch(["id", "email"]);
+  const inputChecker = watcher.includes("") || watcher.includes(undefined) || Object.values(errors)[0];
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    // 여기다가 이제 백에 데이터 보내는거
+  };
+
+  return (
+    <Box position>
+      <CustomForm onSubmit={handleSubmit(onSubmit)}>
+        <Typography marginBottom="4px">ID</Typography>
+        <TextField marginBottom={errors.id?.message ? "6px" : "20px"} onChange={spaceRemover} {...register("id")} />
+        <Typography margin="0px" color="#DF5659" size="14px">
+          {errors.id?.message}
+        </Typography>
+        <Typography marginBottom="4px">EMAIL</Typography>
+        <TextField
+          marginBottom={errors.email?.message ? "6px" : "20px"}
+          onChange={spaceRemover}
+          {...register("email")}
+        />
+        <Typography margin="0px" color="#DF5659" size="14px">
+          {errors.email?.message}
+        </Typography>
+        <Button marginTop="20px" marginBottom="40px" disabled={inputChecker}>
+          FIND ID
+        </Button>
+      </CustomForm>
+    </Box>
+  );
+}
 
 function Find(props) {
   const counter = props.counter;
-  console.log(counter);
   const fadeIn = counter.current === 1 ? "1s" : "";
   const delay = counter.current === 1 ? "1.5s" : "";
   counter.current++;
 
   return (
     <Box fadeIn={fadeIn} delay={delay}>
-      <Box position>
-        <CustomForm margin="30px auto">
-          <Typography>ID</Typography>
-          <TextField />
-          <Button margin="30px auto">FIND ID</Button>
-        </CustomForm>
-        <Typography color="#DF5659" margin={true ? "" : "18px"}>
-          {true}
-        </Typography>
-        <CustomForm margin="30px auto">
-          <Typography>ID</Typography>
-          <TextField />
-          <Typography>EMAIL</Typography>
-          <TextField />
-          <Button margin="30px auto">FIND PASSWORD</Button>
-        </CustomForm>
-        <Typography color="#DF5659" margin={true ? "" : "18px"}>
-          {true}
-        </Typography>
-      </Box>
+      <FindIdBox />
+      <FindPasswordBox />
       <Box position direction="row">
         <Button margin="10px">
           <StyledLink to="../signin">SIGN IN</StyledLink>
