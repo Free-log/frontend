2f8ca489aec6532bcb4d52196182c93ec340c667
Ryokuman <ryokuman21@gmail.com>
2022-11-12 23:31:25 +0900

    chore ( utils ) : change regex for password

--- frontend/src/utils/regexs.js
+++ frontend/src/utils/regexs.js
@@ -1,5 +1,4 @@
-const passwordRegular =
-  /^(?=.*[^ㄱ-ㅎ|가-힣])(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{8,}$/gm;
+const passwordRegular = /^(?=.*[^ㄱ-ㅎ|가-힣])(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{10,}$/gm;
 const idRegular = /^[aA-zZ\d]+$/gm;
 const nickNameRegular = /[aA-zZ\d]+/gm;
 
--- frontend/src/utils/signUpValidator.js
+++ frontend/src/utils/signUpValidator.js
@@ -5,7 +5,7 @@ const signUpValidator = object().shape({
   id: string().min(5).max(20).matches(idRegular, "Only alphanumeric characters are allowed for ID").required(),
   email: string().email().required(),
   password: string()
-    .matches(passwordRegular, "비밀번호는 특수문자 ,숫자 ,영어 대,소문자를 각각 1개씩 포함하고, 8자 이상이여야 합니다")
+    .matches(passwordRegular, "password should contain at least one digit one character and one punctuation")
     .required(),
   passwordConfirm: string()
     .oneOf([ref("password"), null])
