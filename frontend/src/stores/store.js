fd0e83a0537386a86a63f39a5f258c60f9d1146e
Ryokuman <ryokuman21@gmail.com>
2022-11-12 23:23:52 +0900

    feature ( store ) : add fieEffectSlice
    add state for handle animations ( file Effect Slice )

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tokenSlice from "./tokenSlice";
+import fileEffectSlice from "./fileEffectSlice";
 
 export const store = configureStore({
-  reducer: { token: tokenSlice.reducer },
+  reducer: { token: tokenSlice.reducer, fileEffect: fileEffectSlice.reducer },
 });
